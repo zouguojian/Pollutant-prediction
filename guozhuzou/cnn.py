0dbd7d7c1f5aedb203cc4c8cac3a60cb8f0b26ae
GuojianZou <32932644+zouguojian@users.noreply.github.com>
2022-03-11 00:43:45 +0800

    first

--- guozhuzou/cnn.py
+++ guozhuzou/cnn.py
@@ -2,21 +2,18 @@
 
 import tensorflow as tf
 
-class cnn_lstm(object):
-    def __init__(self, batch_size, layer_num=1, nodes=128, highth=3, width=3, placeholders=None):
+class CnnClass(object):
+    def __init__(self, hp=None, placeholders=None):
         '''
         :param batch_size:
         :param layer_num:
         :param nodes:
         :param is_training:
         '''
-        self.batch_size=batch_size
-        self.layer_num=layer_num
-        self.nodes=nodes
+        self.batch_size=hp.batch_size
+        self.h=hp.h
+        self.w=hp.w
         self.placeholders=placeholders
-        self.h=highth
-        self.w=width
-        self.encoder()
 
     def cnn(self,x):
         '''
@@ -72,49 +69,3 @@ class cnn_lstm(object):
 
             # print('cnn output shape is : ',s.shape)
         return s
-
-    def encoder(self):
-        '''
-        :return:  shape is [batch size, time size, hidden size]
-        '''
-
-        def cell():
-            lstm_cell=tf.nn.rnn_cell.BasicLSTMCell(num_units=self.nodes)
-            lstm_cell_=tf.nn.rnn_cell.DropoutWrapper(cell=lstm_cell,output_keep_prob=1.0)
-            return lstm_cell_
-        self.e_mlstm=tf.nn.rnn_cell.MultiRNNCell([cell() for _ in range(self.layer_num)])
-        self.initial_state = self.e_mlstm.zero_state(self.batch_size, tf.float32)
-
-    def encoding(self, inputs, emb):
-        '''
-        :param inputs:
-        :return: shape is [batch size, time size, hidden size]
-        '''
-
-        # for cnn to extract spatial features
-        cnn_out=list()
-        for time in range(inputs.shape[1]):
-            cnn_out.append(self.cnn(tf.expand_dims(inputs[:,time,:,:],axis=-1)))
-
-        inputs=tf.transpose(cnn_out,[1,0,2])
-        inputs =tf.add(inputs,emb)
-
-        #
-        # for i in range(self.batch_size):
-        #     cnn_out.append(tf.expand_dims(inputs[i,:,4,:],axis=0))
-        # inputs = tf.concat(cnn_out,axis=0)
-
-        print('the cnn output series shape is ; ', inputs.shape)
-
-        # out put the store data
-        with tf.variable_scope('encoder_cnn_lstm'):
-            self.h_states, self.c_states = tf.nn.dynamic_rnn(cell=self.e_mlstm, inputs=inputs, initial_state=self.initial_state,dtype=tf.float32)
-
-        return self.h_states, self.c_states
-
-import numpy as np
-if __name__ == '__main__':
-    train_data=np.random.random(size=[32,24,10])
-    x=tf.placeholder(tf.float32, shape=[32, 24, 9, 10])
-    r=cnn_lstm(batch_size=32,layer_num=1,nodes=128,highth=3,width=3)
-    hs=r.encoding(x)
