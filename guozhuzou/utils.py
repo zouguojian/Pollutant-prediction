f260d9d1ec7b0886a7f7eda2d885d3cb1c63905a
GuojianZou <32932644+zouguojian@users.noreply.github.com>
2022-10-17 20:52:26 +0800

    first

--- guozhuzou/utils.py
+++
@@ -1,8 +0,0 @@
-# -- coding: utf-8 --
-
-def construct_feed_dict(features, labels, placeholders):
-    """Construct feed dictionary."""
-    feed_dict = dict()
-    feed_dict.update({placeholders['labels']: labels})
-    feed_dict.update({placeholders['features']: features})
-    return feed_dict
